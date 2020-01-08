b9b4a5f4b3930cc12f8242d2d0774efcf05e108b
owin2828 <owin2828@gmail.com>
2020-01-08 03:51:28 +0000

    Updating to owin2828/my-blog@ca2c97e13a197c5ad2bef8bcb4122d33829a563b.

--- js/search.js
+++ js/search.js
@@ -15,7 +15,7 @@
   
         searchResults.innerHTML = appendString;
       } else {
-        searchResults.innerHTML = '<li>No results found</li>';
+        searchResults.innerHTML = '<li>검색 결과가 없습니다</li>';
       }
     }
   
